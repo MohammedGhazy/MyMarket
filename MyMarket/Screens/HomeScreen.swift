e46eb5956d4b53a810ff79d9e6ee92836180c43d
mohammed <me6636366@gmail.com>
2021-04-29 01:35:12 +0200

    edits

--- MyMarket/Screens/HomeScreen.swift
+++ MyMarket/Screens/HomeScreen.swift
@@ -7,60 +7,45 @@
 //
 
 import UIKit
+import iCarousel
 
 class HomeScreen: UIViewController {
-    
-    let carouselCard            = CarouselCardModel()
-    let pageController          = UIPageControl()
-    let carouselImage           = UIImageView()
-    var collectionView: UICollectionView!
+    let carouselModel = CarouselCardModel()
+    let myCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .timeMachine
+        view.isPagingEnabled = true
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //configureCollectioView()
+        configureMyCarousel()
     }
     
-    func configureCollectioView(){
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection  = .horizontal
-        layout.itemSize         = CGSize(width: view.frame.width, height: view.frame.height)
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
-        collectionView.automaticallyAdjustsScrollIndicatorInsets = false
-        collectionView.setContentOffset(.zero, animated: true)
-        collectionView.backgroundColor  = .systemGreen
-        collectionView.isPagingEnabled  = true
-        collectionView.delegate         = self
-        collectionView.dataSource       = self
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.register(CarouselCell.self, forCellWithReuseIdentifier: CarouselCell.reuseID)
+    func configureMyCarousel(){
+        myCarousel.dataSource = self
+        myCarousel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myCarousel)
+        myCarousel.frame = CGRect(x: 0, y: view.frame.size.height*0.18, width: view.frame.size.width, height: view.frame.size.height*0.2)
         
-        view.addSubview(collectionView)
-        collectionView.addSubview(carouselImage)
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -20),
-            collectionView.heightAnchor.constraint(equalToConstant: 200),
-        ])
     }
 }
 
-extension HomeScreen: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UIScrollViewDelegate, UIPageViewControllerDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension HomeScreen: iCarouselDataSource{
+    func numberOfItems(in carousel: iCarousel) -> Int {
         return 3
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCell.reuseID, for: indexPath) as! CarouselCell
-        cell.carouselImage.image   = carouselCard.carouselImage[indexPath.row]
-        cell.layer.cornerRadius  = 0.6
-        return cell
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width/1.3, height: self.view.frame.size.height*0.2))
+        view.backgroundColor = .systemGreen
+        let imageView = UIImageView(frame: view.bounds)
+        view.addSubview(imageView)
+        imageView.contentMode = .scaleToFill
+        imageView.image       = UIImage(named: "vig\(index+1)")
+        return view
     }
     
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
-    }
+    
 }
